41ef4bc112f01a83dd265e95fa5e3c007a29cf4a
Angatupyry <fierrofenix@gmail.com>
2022-08-22 20:11:02 -0400

    Adding clase 4

---
+++ clase4/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Greeting from "./components/Greeting";
+import TaskList from "./components/TaskList";
+import Counter from "./components/Counter";
+import TreeImage from "./components/TreeImage";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [showThings, setShowThings] = useState(false);
+
+  const showOrHide = () => {
+    setShowThings(!showThings);
+  };
+
+  return (
+    <div className="App">
+      <span>My first react app</span>
+      <Greeting />
+      <TaskList />
+      <Counter />
+      <TreeImage showImage={showThings} showOrHideImage={showOrHide} />
+    </div>
+  );
+}
+
+export default App;
