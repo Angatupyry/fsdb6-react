1c9c19225fbf79f1f71554b9be357705ec6b3bf7
Angatupyry <fierrofenix@gmail.com>
2022-08-24 11:20:48 -0400

    Semaforo en app.js

--- clase5/src/App.js
+++ clase5/src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
-import Semaforo from "./components/Semaforo";
 import { useState } from "react";
 
 function App() {
   const colors = ["red", "yellow", "green"];
 
+  const [colorSemaforo, setColorSemaforo] = useState("grey");
+
+  const funcionQueCambiaColor = (itemParametro) => {
+    setColorSemaforo(itemParametro);
+  };
+
+  /**
+   * colorSemaforo = gris
+   * item = rojo
+   * colorSemaforo = rojo
+   */
+
   return (
     <div className="App">
-      {colors.map((item) => {
-        return <Semaforo color={item} />;
+      {colors.map((item, index) => {
+        return (
+          <div key={index}>
+            <div
+              className="light"
+              style={{
+                backgroundColor: item === colorSemaforo ? item : "grey",
+              }}
+              onClick={() => funcionQueCambiaColor(item)}
+            ></div>
+          </div>
+        );
       })}
     </div>
   );
